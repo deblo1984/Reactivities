3bdacda84b53b0f54ee0facab07008e1f98fb9de
deblo1984 <bintang1984@gmail.com>
2021-03-10 22:04:24 +0700

    Add Formik

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/layout/App';
-import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css'
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'react-calendar/dist/Calendar.css'
+import 'react-datepicker/dist/react-datepicker.css'
+import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { Router } from 'react-router-dom';
