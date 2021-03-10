3fbf564e9d5af296d13bd735d741d47d55d3e04d
deblo1984 <bintang1984@gmail.com>
2021-03-10 22:25:15 +0700

    Startup change

--- API/Startup.cs
+++ API/Startup.cs
@@ -63,9 +63,9 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
             if (env.IsDevelopment())
             {
-                app.UseMiddleware<ExceptionMiddleware>();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
